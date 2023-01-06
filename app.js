f627f179c910e2789275a78022c99bc15809cb73
nsikakudoh <nsikak@sparknerds.io>
2023-01-06 12:56:28 -0700

    items deletes on click

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-let ourForm = document.getElementById("ourForm")
-let ourField = document.getElementById("ourField")
-let ourList = document.getElementById("ourList")
+let ourForm = document.getElementById("ourForm");
+let ourField = document.getElementById("ourField");
+let ourList = document.getElementById("ourList");
 
-ourForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    createItem(ourField.value)
-})
+ourForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createItem(ourField.value);
+});
 
-function createItem(x){
-    let ourHTML =  `<li>${x} <button>Delete</button></li>`
-    ourList.insertAdjacentHTML("beforeend", ourHTML)
+function createItem(x) {
+  let ourHTML = `<li>${x} <button onclick="deleteItem(this)">Delete</button></li>`;
+  ourList.insertAdjacentHTML("beforeend", ourHTML);
+  ourField.value = "";
+  ourField.focus();
+}
 
+function deleteItem(elementToDelete){
+    elementToDelete.parentElement.remove()
+    
 }
